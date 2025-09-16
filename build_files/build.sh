64c56bf8411dee9cb3b7eaf95cbc6dcc78f362ae
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2025-09-16 00:32:11 -0400

    Refactor build.sh to improve package installation process and manage /opt directory more effectively
    Signed-off-by: SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,11 +11,15 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y libratbag-ratbagd 
+mv /opt{,.bak}
+mkdir /opt
 dnf5 install -y "https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm"
 mv /opt/Bitwarden /usr/lib/Bitwarden
 ln -sf /usr/lib/Bitwarden/bitwarden-app /usr/bin/bitwarden-app
 chmod 4755 /usr/lib/Bitwarden/chrome-sandbox
 sed -i 's|^Exec=/opt/Bitwarden|Exec=/usr/bin|g' /usr/share/applications/bitwarden.desktop
+rmdir /opt
+mv /opt{.bak,}
 dnf5 install -y --enablerepo=rpmfusion-nonfree-steam steam-devices mangohud gamescope steam
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
