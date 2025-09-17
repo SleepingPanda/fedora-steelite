69bfbfca6ab5a1978ca48ae9db78e5629cbafc1a
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2025-09-17 02:32:15 -0400

    Refactor build.sh to clean up comments and begin work on enabling nvidia
    Signed-off-by: SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,10 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
+# libratbag
 dnf5 install -y libratbag-ratbagd 
+systemctl enable ratbagd.service
+# Bitwarden
 mv /opt{,.bak}
 mkdir /opt
 dnf5 install -y "https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm"
@@ -21,18 +16,15 @@ chmod 4755 /usr/lib/Bitwarden/chrome-sandbox
 sed -i 's|^Exec=/opt/Bitwarden|Exec=/usr/bin|g' /usr/share/applications/bitwarden.desktop
 rmdir /opt
 mv /opt{.bak,}
-dnf5 install -y --enablerepo=rpmfusion-nonfree-steam steam-devices mangohud gamescope steam
+# Visual Studio Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable ratbagd.service
+# Steam
+dnf5 install -y --enablerepo=rpmfusion-nonfree-steam mangohud gamescope steam
+# tools for building and enabling akmod keys
+dnf5 install -y rpmdevtools akmods
+# NVIDIA driver
+dnf5 install -y --enablerepo=rpmfusion-nonfree --enablerepo=rpmfusion-free akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda libva-nvidia-driver
+# Update kernel arguments to activate driver, disable nouveau and nova, and disable kernel mitigations
+dnf5 kernel-args add "rd.driver.blacklist=nouveau,nova_core" "modprobe.blacklist=nouveau,nova_core" "nvidia-drm.modeset=1" "mitigations=off"
