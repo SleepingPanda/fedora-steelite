d5058d4e9d7590cd0f8b4bafcc54cc6319c320eb
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2025-10-09 10:08:40 -0400

    Add ksshaskpass to Misc Tools installation in build script
    Signed-off-by: SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,7 @@ set -eoux pipefail
 # libratbag
 dnf5 -y install libratbag-ratbagd
 systemctl enable ratbagd.service
+
 # Bitwarden
 mv /opt{,.bak}
 mkdir /opt
@@ -16,13 +17,17 @@ chmod 4755 /usr/lib/Bitwarden/chrome-sandbox
 sed -i 's|^Exec=/opt/Bitwarden|Exec=/usr/bin|g' /usr/share/applications/bitwarden.desktop
 rmdir /opt
 mv /opt{.bak,}
+
 # Visual Studio Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e '[vscode]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=0\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 -y install --enablerepo=vscode code
+
 # Steam
 dnf5 -y install --enablerepo=rpmfusion-nonfree-steam mangohud gamescope steam
+
 # Misc Tools
-dnf5 -y install rpmdevtools akmods
+dnf5 -y install rpmdevtools akmods ksshaskpass
 dnf5 -y config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=1
+
 # Misc Fixes
