95ed5be8670f5cdd65c11a0bf3a07c09c0d5e2a6
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2025-10-18 12:11:05 -0400

    Try adding NVIDIA one more time. (Please work!)
    Make repo details easy to read.
    
    Rework the removals section.
    
    Add a helper function for installing certain apps.
    
    Move all the services to their own section and add podman-auto-update timer.
    
    Signed-off-by: SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,60 +1,98 @@
 #!/bin/bash
 
-set -eoux pipefail
+set -eEox pipefail
+
+# Helper Function
+install_app() {
+    local url="$1"
+    local name="$2"
+
+    local tmpdir
+    tmpdir=$(mktemp -d)
+    mkdir "$tmpdir/opt"
+    mount --bind "$tmpdir/opt" /opt
+    dnf5 -y install "$url"
+    umount /opt
+
+    mv "$tmpdir/opt/$name" "/usr/lib/$name"
+    ln -sfn "/usr/lib/$name" "/opt/$name"
+    rm -rf "$tmpdir"
+}
+
+# NVIDIA
+KERNEL_VERSION="$(rpm -q --queryformat="%{EVR}.%{ARCH}" kernel-core)"
+curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/main/refs/heads/main/build_files/nvidia-install.sh
+chmod +x /tmp/nvidia-install.sh
+IMAGE_NAME="kinoite" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
+rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
+dnf5 config-manager setopt fedora-multimedia.enabled=1 fedora-nvidia.enabled=0
+
+depmod -a "${KERNEL_VERSION}"
 
 # Misc Tools
-dnf5 -y install rpmdevtools akmods ksshaskpass libva-nvidia-driver gstreamer1-plugin-openh264
-dnf5 -y config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=1
+dnf5 -y install ksshaskpass gstreamer1-plugin-openh264
 
 # Misc Removals
-dnf5 -y remove alsa-firmware alsa-sof-firmware amd-gpu-firmware atheros-firmware brcmfmac-firmware cirrus-audio-firmware intel-audio-firmware intel-gpu-firmware intel-vsc-firmware iwlegacy-firmware iwlwifi-dvm-firmware iwlwifi-mld-firmware iwlwifi-mvm-firmware libertas-firmware mt7xxx-firmware nxpwireless-firmware qcom-wwan-firmware tiwilink-firmware thermald firefox
+dnf5 -y remove '*-firmware' thermald firefox \
+    --exclude='nvidia-gpu-firmware' \
+    --exclude='amd-ucode-firmware' \
+    --exclude='linux-firmware*' \
+    --exclude='realtek-firmware'
 
 # FFMPEG and Codecs
-echo -e '[rpmfusion-free]\nname=RPM Fusion for Fedora $releasever\nbaseurl=http://download1.rpmfusion.org/free/fedora/releases/$releasever/Everything/$basearch/os/\nenabled=0\ngpgcheck=1\ngpgkey=file:///usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-fedora-$releasever' | tee /etc/yum.repos.d/rpmfusion-free.repo > /dev/null
+tee /etc/yum.repos.d/rpmfusion-free.repo <<'EOF'
+[rpmfusion-free]
+name=RPM Fusion for Fedora $releasever
+baseurl=http://download1.rpmfusion.org/free/fedora/releases/$releasever/Everything/$basearch/os/
+enabled=0
+gpgcheck=1
+gpgkey=file:///usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-fedora-$releasever
+EOF
 dnf5 -y swap ffmpeg-free --enablerepo=rpmfusion-free ffmpeg --allowerasing
 
 # libratbag
 dnf5 -y install libratbag-ratbagd
-systemctl enable ratbagd.service
 
 # Steam
 dnf5 -y install --enablerepo=rpmfusion-nonfree-steam mangohud gamescope steam
 
 # Docker CE
 rpm --import https://download.docker.com/linux/fedora/gpg
-echo -e '[docker-ce]\nname=Docker CE\nbaseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable\nenabled=0\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/fedora/gpg' | tee /etc/yum.repos.d/docker-ce.repo > /dev/null
+tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
+[docker-ce]
+name=Docker CE
+baseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable
+enabled=0
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
 dnf5 -y install --enablerepo=docker-ce docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable docker
-systemctl enable containerd
 echo -e 'g docker 998' | tee /usr/lib/sysusers.d/docker.conf > /dev/null
 
 # Bitwarden
-mv /opt{,.bak}
-mkdir /opt
-dnf5 -y install "https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm"
-mv /opt/Bitwarden /usr/lib/Bitwarden
-ln -sf /usr/lib/Bitwarden/bitwarden /usr/bin/bitwarden
-ln -sf /usr/lib/Bitwarden/bitwarden-app /usr/bin/bitwarden-app
+install_app "https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm" Bitwarden
 chmod 4755 /usr/lib/Bitwarden/chrome-sandbox
-sed -i 's|^Exec=/opt/Bitwarden|Exec=/usr/bin|g' /usr/share/applications/bitwarden.desktop
-rmdir /opt
-mv /opt{.bak,}
 
 # Visual Studio Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e '[vscode]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=0\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[vscode]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=0
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
 dnf5 -y install --enablerepo=vscode code
 
 # WinBoat
-mv /opt{,.bak}
-mkdir /opt
-dnf5 -y install "https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x86_64.rpm"
-mv /opt/winboat /usr/lib/winboat
-ln -sf /usr/lib/winboat/winboat /usr/bin/winboat
+install_app "https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x86_64.rpm" winboat
 chmod 4755 /usr/lib/winboat/chrome-sandbox
-sed -i 's|^Exec=/opt/winboat|Exec=/usr/bin|g' /usr/share/applications/winboat.desktop
-rmdir /opt
-mv /opt{.bak,}
+
+# Enable Services
+systemctl enable ratbagd.service docker.service containerd.service
+systemctl --global enable podman-auto-update.timer
 
 dnf5 -y clean all
 rm -rf /var/lib/dnf
