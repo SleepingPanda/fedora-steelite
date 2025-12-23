4dc6d275e733a5124e4c373154f1677d1b5f4d6d
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2025-12-23 00:15:34 -0400

    Update package installation in build script to include glycin-thumbnailer
    Signed-off-by: SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -49,7 +49,7 @@ EOF
 # Install Packages
 dnf5 -y config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=1
 dnf5 -y swap ffmpeg-free --enablerepo=rpmfusion-free ffmpeg --allowerasing
-dnf5 -y install --enablerepo=docker-ce --enablerepo=lact --enablerepo=rpmfusion-free --enablerepo=rpmfusion-nonfree-steam --enablerepo=vscode akmods android-tools gamescope ksshaskpass libratbag-ratbagd mangohud rpmdevtools python3-pip python3-pyicu containerd.io docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin lact libva-nvidia-driver gstreamer1-plugin-openh264 gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi steam code
+dnf5 -y install --enablerepo=docker-ce --enablerepo=lact --enablerepo=rpmfusion-free --enablerepo=rpmfusion-nonfree-steam --enablerepo=vscode akmods android-tools gamescope glycin-thumbnailer ksshaskpass libratbag-ratbagd mangohud rpmdevtools python3-pip python3-pyicu containerd.io docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin lact libva-nvidia-driver gstreamer1-plugin-openh264 gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi steam code
 
 mv /opt{,.bak}
 mkdir /opt
